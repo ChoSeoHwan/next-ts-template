5b6019b0639a7b15489c05bd893d0c04027d0c8a
wtae1216 <wtae1216@gmail.com>
2020-11-08 17:11:13 +0900

    버튼 컴포넌트 테스트 추가

--- src/components/atoms/Button.test.tsx
+++ src/components/atoms/Button.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/dom';
 
 import render from 'libs/testUtils';
 
@@ -11,5 +12,16 @@ describe('Components | Atoms | <Button />', () => {
         const { getByText } = render(<Button>{text}</Button>);
 
         expect(getByText(text)).toHaveTextContent(text);
+    });
+
+    it('should active onClick event', () => {
+        const handleClick = jest.fn();
+        const { getByText } = render(
+            <Button onClick={handleClick}>{text}</Button>
+        );
+
+        fireEvent.click(getByText(text));
+
+        expect(handleClick).toBeCalled();
     });
 });
